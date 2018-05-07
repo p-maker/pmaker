ea721893fd55617168bc35eb17ee6f2cb958cff3
Dmitry Sayutin <cdkrot@yandex.ru>
2018-05-07 21:02:59 +0300

    minor fix with setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
 from setuptools import setup, find_packages
-
+from codecs import open
 import os, os.path
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
@@ -14,7 +14,8 @@ setup(
     long_description_content_type='text/markdown',
     url = "https://github.com/p-maker/pmaker",
     author = "Dmitry Sayutin",
-    
+
+    python_requires = ">=3.5, <4",
     setup_requires = ['setuptools_scm', 'jinja2', 'python_version>="3.5"'],
     use_scm_version = {"local_scheme": "dirty-tag"},
 
