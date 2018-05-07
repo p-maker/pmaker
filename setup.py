32c90178993ceba03b033592f673702d9b61222e
Dmitry Sayutin <cdkrot@yandex.ru>
2018-05-07 20:46:18 +0300

    add license and some meta to the setup

--- setup.py
+++ setup.py
@@ -2,8 +2,19 @@
 
 from setuptools import setup, find_packages
 
+import os, os.path
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = "pmaker",
+    description = "Toolkit for creating problems for programming competitions",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url = "https://github.com/p-maker/pmaker",
+    author = "Dmitry Sayutin",
+    
     setup_requires = ['setuptools_scm', 'jinja2', 'python_version>="3.5"'],
     use_scm_version = {"local_scheme": "dirty-tag"},
 
